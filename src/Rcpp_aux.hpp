aa53ff8aaab8a0221a18076bfa1f510c8b3dc61c
daviddai80 <daimingwei@gmail.com>
2018-07-11 01:46:13 +0000

    Update Rcpp_aux.hpp
    fix bugs 1.0

--- src/Rcpp_aux.hpp
+++ src/Rcpp_aux.hpp
@@ -262,7 +262,7 @@ void preprocess_summary(SEXP Xs, SEXP  SS, double lbPval, int& type, int& N, int
     int* p = INTEGER(Xs);
     Mat<int> mat_X(p, N, P, false);
     summary = combine_summary_X(mat_X, xsnps, SS, lbPval);
-    bool create_new_matrix = false;
+    bool create_new_matrix = true;
     if(mat_X.n_cols < P){
       create_new_matrix = true;
       P = mat_X.n_cols;
@@ -273,7 +273,7 @@ void preprocess_summary(SEXP Xs, SEXP  SS, double lbPval, int& type, int& N, int
     double* p = REAL(Xs);
     Mat<double> mat_X(p, N, P,false);
     summary = combine_summary_X(mat_X, xsnps, SS, lbPval);
-    bool create_new_matrix = false;
+    bool create_new_matrix = true;
     if(mat_X.n_cols < P){
         create_new_matrix = true;
         P = mat_X.n_cols;
